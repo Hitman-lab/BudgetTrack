37c4941ae5ebffafb5f6b10a820b55ffabd7c1e4
Desktop-PC <hackzs@outlook.com>
2020-02-29 17:31:28 +0530

    Updated the UI functionality

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ var budgetController = (function() {
 		this.description = description;
 		this.value = value;
 	};
-
+	
 	var Income = function(id, description, value){
 		this.id = id;
 		this.description = description;
 		this.value = value;
 	};
-
+	
 	var calculateTotal = function(type){
-
-		
-
-
+		var sum = 0;
+		data.allItems[type].forEach( function(cur) {
+			sum = sum + cur.value;
+		});
+		data.totals[type] = sum;
 	};
 
 	var data = {
@@ -30,7 +31,10 @@ var budgetController = (function() {
 		totals: {
 			exp: 0,
 			inc: 0
-		}
+		}, 
+
+		budget: 0,
+		percentage: -1
 	};
 
 	return{
@@ -42,8 +46,7 @@ var budgetController = (function() {
 				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
 			}else{
 				ID = 0;
-			}
-				
+			}	
 			// create new item based on 'inc' or 'exp' type
 			if(type === 'exp'){
 				newItem = new Expenses(ID, des, val);
@@ -54,6 +57,33 @@ var budgetController = (function() {
 			// then push it into the data structure
 			data.allItems[type].push(newItem);
 			return newItem; // return the new item
+		},
+
+		calculateBudget: function() {
+			// calculate the total income and expenses
+			calculateTotal('exp');
+			calculateTotal('inc');
+
+			// calculate the budget (income - expenses)
+			data.budget = data.totals.inc - data.totals.exp;
+			
+			// calculate the percentage of income that we spent
+
+			if(data.totals.inc > 0){
+				data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+			}else{
+				data.percentage = -1;
+			}
+
+		},
+
+		getBudget: function() {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage
+			};	
 		},
 
 		testing: function(){
@@ -72,7 +102,11 @@ var UIController = (function() {
 		inputValue: '.add_value',
 		inputBtn: '.add_btn',
 		incomeContainer: '.income_list',
-		expensesContainer: '.expenses_list'
+		expensesContainer: '.expenses_list',
+		budgetLabel: '.budget_value',
+		incomeLabel: '.budget_income--value',
+		expensesLabel: '.budget_expenses--value',
+		percentageLabel: '.budget_expenses--percentage',
 	};
 	
 	return{
@@ -137,6 +171,18 @@ var UIController = (function() {
 			// focus back to the description box
 			fieldsArray[0].focus();
 			fieldsArray[0].style.border ='1px solid red';
+		},
+
+		displayBudget: function(obj) {
+			document.querySelector(DOMStrings.budgetLabel).textContent = '+ ' + obj.budget;
+			document.querySelector(DOMStrings.incomeLabel).textContent = '+ ' + obj.totalInc;
+			document.querySelector(DOMStrings.expensesLabel).textContent = '- ' + obj.totalExp;
+
+			if(obj.percentage > 0){
+				document.querySelector(DOMStrings.percentageLabel).textContent = obj.percentage + '%';
+			}else{
+				document.querySelector(DOMStrings.percentageLabel).textContent = '---';
+			}
 		}
 	};
 })();
@@ -167,11 +213,13 @@ var appController = (function(budgetCtrl, UICtrl) {
 	var updateBudget = function(){
 
 		// 1. Calculate the budget
+		budgetCtrl.calculateBudget();
 
 		// 2. return the budget
+		var budget = budgetCtrl.getBudget();
 
 		// 3. Display the budget on the UI
-
+		UICtrl.displayBudget(budget);
 	};
 
 
@@ -180,7 +228,7 @@ var appController = (function(budgetCtrl, UICtrl) {
 		// 1. Get the field input data
 		var inputs = UICtrl.getInput();
 		
-		if(inputs.description !== '' && !isNAN(inputs.value) && inputs.value > 0 ){
+		if(inputs.description !== '' && !isNaN(inputs.value) && inputs.value > 0 ){
 
 			// 2. Add the item to the budget controller
 			var newItems = budgetCtrl.addItem(inputs.type, inputs.description, inputs.value);
@@ -200,6 +248,14 @@ var appController = (function(budgetCtrl, UICtrl) {
 	return{
 		init: function() {
 			console.log('application has started.');
+			UICtrl.displayBudget(
+				{
+					budget: 0,
+					totalInc: 0,
+					totalExp: 0,
+					percentage: -1
+				}
+			);
 			setupEventListener();
 		}
 	}
